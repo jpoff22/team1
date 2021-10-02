665580d93c4b41b6fc1a0a4e974ba2544f299377
Liam DeVoe <orionldevoe@gmail.com>
2021-10-02 17:59:09 -0400

    implement get_valid_moves

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -15,7 +15,21 @@ public class PacMan{
 	}
 
 	public ArrayList<Location> get_valid_moves() {
-		return null;	
+		Location up = myLoc.shift(0, 1);
+		Location down = myLoc.shift(0, -1);
+		Location left = myLoc.shift(-1, 0);
+		Location right = myLoc.shift(1, 0);
+
+		ArrayList<Location> validMoves = new ArrayList<Location>();
+
+		for (Location loc : new Location[]{up, down, left, right}) {
+			if (myMap.getLoc(loc).contains(Map.Type.WALL)) {
+				continue;
+			}
+			validMoves.add(loc);
+		}
+
+		return validMoves;
 	}
 
 	public boolean move() {
