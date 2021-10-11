2253ad52a0caea132e4fee828136ca6b0427e313
Mathias Hailu <math4@terpmail.umd.edu>
2021-10-10 23:33:32 -0400

    implemented pacman-consume

--- Projects/P2/src/PacMan.java
+++ Projects/P2/src/PacMan.java
@@ -27,6 +27,11 @@ public class PacMan{
 	}
 
 	public JComponent consume() { 
+
+		if (myMap.getLoc(myLoc).contains(Map.Type.COOKIE)) {
+			return myMap.eatCookie("pacman");
+		}
+
  		return null;
 	}
 }
