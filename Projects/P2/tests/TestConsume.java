802aeabf34e5c3290c3d4de4de67b39b594f0040
Mathias Hailu <math4@terpmail.umd.edu>
2021-10-11 23:31:25 -0400

    implemented tests for pacman-consume

--- Projects/P2/tests/TestConsume.java
+++ Projects/P2/tests/TestConsume.java
@@ -3,8 +3,30 @@ import java.awt.Color;
 import java.io.*;
 
 public class TestConsume extends TestCase {
-
+	@Test
 	public void testConsume() throws FileNotFoundException {
-		return null;	
-	}
+		
+        	NoFrame frame= null;
+			try {
+				frame= new NoFrame();
+				
+			} catch (FileNotFoundException e) {
+				System.out.println(e);
+			}
+			Map map= frame.getMap();
+
+			//initialize a pacman at (9,12) like the README example
+			PacMan mypacman = frame.addPacMan(new Location(9, 12));
+			//should return null because there is no cooke at (9,12)
+			assertNull(mypacman.consume());
+
+			//test trying to consume at a spot with a cookie twice
+			PacMan mypacman2 = frame.addPacMan(new Location(1, 2));
+			//should not return null because there is a cookie
+			assertNotNull(mypacman2.consume());
+			//should be null because there is no longer a cookie
+			assertNull(mypacman2.consume());
+
+		}	
 }
+
