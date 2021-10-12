17651e290c66332e438ee9500dbc2ca04c7c3cdf
Mathias Hailu <math4@terpmail.umd.edu>
2021-10-11 21:46:32 -0400

    trying to write pacman-move test

---
+++ Projects/P2/runtest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#
+# Compile and run a JUnit test - run from this dir
+# 	example: runtest.sh tests/TestMapMove.java
+
+test_path=$1
+test_name=$(grep -oP '(?<=/).*?(?=\.)' <<< $test_path)
+
+echo "===>> compiling test $test_path"
+javac -cp tests/junit-4.10.jar src/*.java $test_path
+if [ $? -eq 0 ]; then
+	echo "===>> running test $test_name"
+	java -cp tests/junit-4.10.jar:tests/:src/ org.junit.runner.JUnitCore ${test_name%.*}
+fi
