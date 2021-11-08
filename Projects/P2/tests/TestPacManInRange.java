3845fee1cfd2480ef772b32797edb06b63519303
mlopez122 <mlopez10@terpmail.umd.edu>
2021-11-08 18:58:29 -0500

    fixed pacman.java, testmapgetloc.java, and testpacmaninrange.java

--- Projects/P2/tests/TestPacManInRange.java
+++ Projects/P2/tests/TestPacManInRange.java
@@ -5,6 +5,10 @@ import java.io.*;
 public class TestPacManInRange extends TestCase {
 
 	public void testPacManInRange() throws FileNotFoundException{
-		return;
+		Ghost ghost = frame.addGhost(new Location(3, 4), "name", Color.red); //Creates a red ghost named "name" at location x,y
+	PacMan pacman = frame.addPacMan(new Location(1, 2)); //Creates PacMan at location x, y
+	assertFalse(ghost.is_pacman_in_range());
+	Ghost ghost2 = frame.addGhost(new Location(2, 2), "name2", Color.blue);
+	assertTrue(ghost2.is_pacman_in_range());
 	}
 }
