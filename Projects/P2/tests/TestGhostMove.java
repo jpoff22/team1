a67abe55824369901a60401efb09701f417f1355
Cyrus <cyruswang2017@gmail.com>
2021-11-08 20:52:45 -0500

    missing frame.startGame()

--- Projects/P2/tests/TestGhostMove.java
+++ Projects/P2/tests/TestGhostMove.java
@@ -10,7 +10,7 @@ public class TestGhostMove extends TestCase {
 
 		Ghost ghost = frame.addGhost(new Location(1, 1), "Blinky", Color.RED);
         Map map = frame.getMap();
-
+		frame.startGame();
 		// ghost only has two places to move, make sure it moved to one of them
 		assertEquals(true, map.getLoc(new Location(1, 2)).contains(Map.Type.GHOST) || map.getLoc(new Location(2, 1)).contains(Map.Type.GHOST));
 	}
